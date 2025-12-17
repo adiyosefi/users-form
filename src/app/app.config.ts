2909e68135f89e6ac802943958df3c0bd8903d25
Amit Yosefi <amit.yosefi@gmail.com>
2025-12-17 12:15:36 +0200

    remove deprecated error styles.scss

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,9 @@ import {ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from "@angular/common/http";
 import {provideNativeDateAdapter} from "@angular/material/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideNativeDateAdapter()]
+  providers: [provideRouter(routes), provideHttpClient(), provideNativeDateAdapter()]
 };
